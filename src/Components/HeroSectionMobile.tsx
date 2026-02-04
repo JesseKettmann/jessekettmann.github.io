b7c8a65a7a8f210b2d8ffed39ada5167cfb285b7
JesseKettmann <jessekettmann@gmail.com>
2026-02-04 13:53:58 +0100

    changed hero section scaling

--- src/Components/HeroSectionMobile.tsx
+++ src/Components/HeroSectionMobile.tsx
@@ -2,52 +2,43 @@ import HeroSectionContent from "./HeroSectionContent";
 
 function HeroSectionMobile() {
   return (
-    <div className="relative mx-auto w-full aspect-18/16 flex justify-center items-end p-global">
-      {/* Image */}
-      <img
-        src="/cover-image.png"
-        alt="Man sitting in chair"
-        className="
-          absolute
-          left-1/2
-          bottom-0
-          -translate-x-1/2
-          translate-y-[28%]
-          w-[90vw]
-          h-auto
-          object-contain
-          z-0
-        "
-      />
+    <div className="relative w-full flex flex-col justify-center p-global overflow-hidden">
+      <div className="absolute left-1/2 -translate-x-1/2 top-0 w-full aspect-39/32 overflow-hidden">
+        {/* Image */}
+        <img
+          src="/cover-image.png"
+          alt="Man sitting in chair"
+          className="
+            absolute
+            left-1/2
+            bottom-0
+            -translate-x-1/2
+            translate-y-[35%]
+            w-[90vw]
+            h-auto
+            object-contain
+            z-0
+          "
+        />
+        {/* Gradient fade */}
+        <div
+          className="
+            absolute
+            inset-x-0
+            bottom-0
+            h-[50%]
+            bg-linear-to-b
+            from-transparent
+            to-background-mint
+            z-10
+          "
+        />
+      </div>
 
-      {/* Gradient fade */}
-      <div
-        className="
-          absolute
-          inset-x-0
-          bottom-[18%]
-          h-[40%]
-          bg-linear-to-b
-          from-transparent
-          to-background-mint
-          z-10
-        "
-      />
-
-      {/* Solid bottom */}
-      <div
-        className="
-          absolute
-          inset-x-0
-          bottom-0
-          h-[18%]
-          bg-background-mint
-          z-10
-        "
-      />
+      <div className="w-full aspect-43/32" />
 
       {/* Content */}
-      <div className="font-dm-sans text-center z-10">
+      <div className="font-dm-sans text-center flex flex-col items-center z-10">
         <HeroSectionContent />
       </div>
     </div>
